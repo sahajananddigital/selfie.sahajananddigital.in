6bb7d0a457e75eba6d6f308fa6dd104e52768fe5
vanpariyar <26689210+vanpariyar@users.noreply.github.com>
2023-04-10 00:15:22 +0530

    FIX: image pixels

--- akman/script.js
+++ akman/script.js
@@ -60,7 +60,7 @@ function mergeSelfie(imgURL, textInput){
 
   mergeImages([
     { src: 'bg.png', x: 0, y: 0 },
-    { src: imgURL, x:105, y: 2870 },
+    { src: imgURL, x:105, y: 2867 },
   ], {nameText: textInput}).then(b64 => imageSrc = b64).then( b64 => {
     aTag.href = imageSrc;
     return b64;
